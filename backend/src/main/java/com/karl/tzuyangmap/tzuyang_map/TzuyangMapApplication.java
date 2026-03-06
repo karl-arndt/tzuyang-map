a03f5221504c625d081c15adc10584ea24c2547e
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2026-03-06 10:54:54 -0500

    Initialize spring boot

---
+++ backend/src/main/java/com/karl/tzuyangmap/tzuyang_map/TzuyangMapApplication.java
@@ -0,0 +1,13 @@
+package com.karl.tzuyangmap.tzuyang_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzuyangMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TzuyangMapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/karl/tzuyangmap/tzuyang_map/TzuyangMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karl.tzuyangmap.tzuyang_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TzuyangMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
