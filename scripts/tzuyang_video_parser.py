9a6e91fce97bfcabdf7b8bc5b85786622b2888d7
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2026-03-10 06:30:07 -0400

    Add channel id getter

--- scripts/tzuyang_video_parser.py
+++ scripts/tzuyang_video_parser.py
@@ -1,8 +1,20 @@
 from dotenv import load_dotenv
 import os
+import googleapiclient.discovery
 
 load_dotenv()
 
 youtube_api_key = os.getenv("YOUTUBE_API_KEY")
+youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=youtube_api_key)
 
-print(f"Your YouTube API Key is: {youtube_api_key}")
+tzuyang_main_channel_name = "@tzuyang6145"
+tzuyang_vlog_channel_name = "@v-tzuyang"
+
+tzuyang_main_id = youtube.channels().list(part="id", forHandle=tzuyang_main_channel_name).execute()["items"][0]["id"]
+tzuyang_vlog_id = youtube.channels().list(part="id", forHandle=tzuyang_vlog_channel_name).execute()["items"][0]["id"]
+
+print(tzuyang_main_id)
+print(tzuyang_vlog_id)
+
+main_videos = youtube.search().list(part="id", channelId=tzuyang_main_id, maxResults=50, type="video").execute()
+print(main_videos)
