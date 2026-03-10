2288026c77d687a4e4290c8e98c63b0dc963f339
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2026-03-10 10:19:25 -0400

    Add playlist getter

--- scripts/tzuyang_video_parser.py
+++ scripts/tzuyang_video_parser.py
@@ -13,8 +13,18 @@ tzuyang_vlog_channel_name = "@v-tzuyang"
 tzuyang_main_id = youtube.channels().list(part="id", forHandle=tzuyang_main_channel_name).execute()["items"][0]["id"]
 tzuyang_vlog_id = youtube.channels().list(part="id", forHandle=tzuyang_vlog_channel_name).execute()["items"][0]["id"]
 
-print(tzuyang_main_id)
-print(tzuyang_vlog_id)
+main_channel_data = youtube.channels().list(part="contentDetails", id=tzuyang_main_id).execute()
+main_uploads_playlist_id = main_channel_data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
-main_videos = youtube.search().list(part="id", channelId=tzuyang_main_id, maxResults=50, type="video").execute()
-print(main_videos)
+page_token = None
+
+while True: 
+    main_videos_raw = youtube.playlistItems().list(part="snippet", playlistId=main_uploads_playlist_id, maxResults=50, pageToken=page_token).execute()
+    main_videos = main_videos_raw["items"]
+    for video in main_videos:
+        video_id = video["snippet"]["resourceId"]["videoId"]
+        video_title = video["snippet"]["title"]
+        print(f"{video_id}: {video_title}")
+    page_token = main_videos_raw.get("nextPageToken")
+    if not page_token:
+        break
